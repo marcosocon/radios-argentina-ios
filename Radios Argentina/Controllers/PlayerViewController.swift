62c83184314c108f73538bb61349dd3b61b81c04
marcosocon <oconmarcos97@gmail.com>
2020-05-19 20:18:10 -0300

    first commit

---
+++ Radios Argentina/Controllers/PlayerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PlayerViewController.swift
+//  Radios Argentina
+//
+//  Created by Marcos Ocon on 19/05/2020.
+//  Copyright © 2020 Marcos Ocon. All rights reserved.
+//
+
+import UIKit
+
+class PlayerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
